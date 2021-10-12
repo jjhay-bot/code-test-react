adb8b4cb68ca088d4daf885eabd7b443195f5c94
jhay <jjhay-bot@github.com>
2021-10-12 14:08:36 +0800

    added clearbutton

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -2,16 +2,26 @@ import React, { useState } from 'react'
 import "./SearchBar.css"
 
 const SearchBar = () => {
-    const [searchValue, setSearchValue] = useState(" ")            // default, updated
+    const [searchValue, setSearchValue] = useState("")            // default, updated
 
     const handleInputChange = (e) => {
         setSearchValue(e.target.value)
     }
 
+    const handleClearClick = () => {
+        setSearchValue("")
+    }
+
+    const displayClear = searchValue.length > 0
+
     return (
         <div>
-            <input type="text" value={searchValue} onChange={handleInputChange} />
-            {searchValue}
+            <i>{searchValue}</i>
+            <br />
+            <input id="coun" type="text" value={searchValue} onChange={handleInputChange} />
+            {displayClear && <button onClick={handleClearClick}>clear</button>}
+            
+
         </div>
     )
    
