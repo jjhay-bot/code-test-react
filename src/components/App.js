ce3b766b88580da58d1110d1dd7db808f469da89
jhay <jjhay-bot@github.com>
2021-10-12 15:02:28 +0800

    review useEffect

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,7 @@ import React from 'react'
 import CountButton from './CountButton/CountButton'
 import Spinner from './Spinner/Spinner'
 import SeachBar from './SearchBar/SearchBar'
-
+import TestUse from './TestUse/TestUse'
 
 const App = () => {
     const myItem = 'jhay'
@@ -14,15 +14,16 @@ const App = () => {
 
             {/* <Spinner /> */}
 
-            <SeachBar products={[ 
+            {/* <SeachBar products={[ 
                 'product 1',
                 'pro2duct 2',
                 'prod3uct 3',
                 'produ4ct 4',
                 ]} 
-            />
+            /> */}
 
-            
+            <TestUse incrementBy={1} />
+    
 
         </div>
     )
---
+++ src/components/TestUse/TestUse.js
@@ -0,0 +1,30 @@
+import React, { useState , useEffect } from 'react'
+import "./TestUse.css"
+
+const TestUse = (props) => {
+    const [currentCount, setCurrentCount] = useState(0)         //userState(defaultvalue)
+
+    
+    useEffect(() => {      
+        console.log(currentCount);                                     // useEffect(() => {}, [])
+        if (currentCount === 10) {
+            // somelogic here ....
+            alert('ops 10')
+            setCurrentCount(0)
+        }
+    }, [currentCount])              // can add multiple states
+   
+
+    const handleClick = () => {
+        setCurrentCount(currentCount + props.incrementBy) 
+    }
+
+    return (
+        <div>
+            <button onClick={handleClick}> +{props.incrementBy} </button>
+            <div className={"count-display"} >{currentCount}</div>
+        </div>
+    )
+}
+
+export default TestUse
